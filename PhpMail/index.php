c4ca2c8a2fc984859721c02fb2a7623611cd298a
Sahil Khosla <khosla419@gmail.com>
2018-01-01 21:07:30 +0530

    Add files via upload

---
+++ PhpMail/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Php Mail</title>
+</head>
+<body>
+<form action="sendemai.php" method="post">
+  First Name:<br><br/>
+  <input type="text" name="fname"><br><br/>
+  Last name:<br><br/>
+  <input type="text" name="lname"><br><br>
+    Email ID:<br><br/>
+  <input type="text" name="email"><br><br>
+    Mobile No:<br><br/>
+  <input type="text" name="mobile"><br><br>
+  <input type="submit" value="Submit">
+</form>
+</body>
+</html>
---
+++ PhpMail/mail.php
@@ -0,0 +1,68 @@
+<?php
+// require_once 'mailerClass/class.php';
+ require_once 'mailerClass/PHPMailerAutoload.php';
+ 
+ $mail = new PHPMailer;
+ 
+ //Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 0;
+
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+	
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "youremail@gmail.com";
+
+//Password to use for SMTP authentication
+$mail->Password = "youremailpassword";
+
+//Set who the message is to be sent from
+$mail->setFrom('donotreply@email.com', 'name');
+
+//Set an alternative reply-to address
+//$mail->addReplyTo('replyto@example.com', 'First Last');
+
+//Set who the message is to be sent to
+$mail->addAddress('toemail@gmail.com', 'name');
+
+//Set the subject line
+$mail->Subject = 'Php Mail Check';
+
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+//$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+
+$mail->Body = "First Name: <b>\"".$_POST['fname']." <br> Last Name: ".$_POST['lname']."<br> Email ID : ".$_POST['email']." <br> Phone No: ".$_POST['mobile']."\"</b>";
+
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+$mail->isHTML(true);  
+//Attach an image file
+//$mail->addAttachment('images/phpmailer_mini.png');
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} 
+header("Location: index.php");
+
+ 
+?>
